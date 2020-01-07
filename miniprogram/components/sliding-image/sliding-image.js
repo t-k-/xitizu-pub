3a1205cb74d9e8d61c1564d92c034e9698d0d652
t-k- <clock126@126.com>
2020-01-07 22:46:08 +0800

    now sliding-image will center / scroll depending on window width.

--- miniprogram/components/sliding-image/sliding-image.js
+++ miniprogram/components/sliding-image/sliding-image.js
@@ -14,7 +14,24 @@ Component({
    * 组件的初始数据
    */
   data: {
+    outer_style: 'width: 100%',
+    inner_style: 'width: 100vw'
+  },
 
+  attached: function () {
+    const win_width = wx.getSystemInfoSync().windowWidth;
+    var vm = this;
+    wx.getImageInfo({
+      src: vm.properties.url,
+      success: function (res) {
+        var min = Math.min(res.width, win_width)
+        console.log(res.width, win_width)
+        vm.setData({
+          outer_style: 'width:' + min + 'px',
+          inner_style: 'width:' + res.width + 'px'
+        })
+      }
+    })
   },
 
   /**
