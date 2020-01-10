5ca8f0e3bbfd12ae9725d6937de9a7a0bdd69a60
t-k- <clock126@126.com>
2020-01-10 16:20:36 +0800

    writing comment editor ...

---
+++ miniprogram/components/comment-input/comment-input.js
@@ -0,0 +1,53 @@
+// components/comment-input/comment-input.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    focused: false,
+    empty: true,
+    content: ''
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onCommentFocus: function (ev) {
+      const content = ev.detail.value.trim()
+      if (content.length == 0) {
+        /* trim field */
+        this.setData({
+          content: ''
+        })
+      }
+
+      this.setData({
+        focused: true
+      })
+    },
+    onCommentBlur: function (ev) {
+      const left_val = ev.detail.value.trim()
+      if (left_val.length == 0) {
+        this.setData({
+          empty: true,
+          content: ''
+        })
+      } else {
+        this.setData({
+          empty: false
+        })
+      }
+      this.setData({
+        focused: false
+      })
+    }
+  }
+})
